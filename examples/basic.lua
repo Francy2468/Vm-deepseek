c8fd4b479ec6fb08e733829fc15d6bdc8ee865b9
Francy2468 <francyertj@gmail.com>
2026-02-11 22:45:09 +0100

    Add basic example for Abyss VM in Lua

---
+++ examples/basic.lua
@@ -0,0 +1,12 @@
+-- ========================================
+-- ABYSS VM - EJEMPLO BÁSICO
+-- ========================================
+
+print("Hola Abyss VM")
+print("Esto corre en NUESTRA máquina virtual")
+
+local a = 42
+local b = 24
+local c = a + b
+
+print("42 + 24 = " .. c)
